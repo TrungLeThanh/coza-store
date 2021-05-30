d7461dc391ceb7a10fee0047c8f4aaa9726183d5
Thanh Trung <thanhtrung20022001@gmail.com>
2021-05-30 18:15:47 +0700

    types fashon

---
+++ frontend/src/components/TypesFashon.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './TypesFashon.css';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles'
+
+const TypesFashon = () => {
+
+    const useStyles = makeStyles({
+        root: {
+            border: 0,
+            color: 'black',
+            height: 40,
+            padding: '0 15px',
+            fontSize: 16,
+            transition: '0.5s',
+            top: 140,
+            left: 15,
+            fontWeight: '400',
+            '&:hover':{
+                borderBottom: '2px solid rgb(108, 122, 224)',
+                background: '#fff',
+            }, 
+            zIndex: 999
+        }
+    });
+
+    const images = [
+        {
+            img: '/images/xbanner-01.webp',
+            title: 'Woman',
+            subTitle: 'Spring 2020'
+        },
+        {
+            img: '/images/xbanner-02.webp',
+            title: 'Man',
+            subTitle: 'Spring 2021'
+        },
+        {
+            img: '/images/xbanner-03.webp',
+            title: 'Accessories',
+            subTitle: 'New trend'
+        }
+    ];
+
+    const classes = useStyles();
+
+    return (
+        <div className="wrap-type-fashon">
+            <Grid container spacing={8}>
+                {images.map(x => {
+                    return <Grid item xs={12} md={6} lg={4} key={x} >
+                        <div className="image-fashon">
+                            <h2 style={{position: 'relative', top: '25px', left: '25px', margin: '0', padding: '0', zIndex: '5'}}>{x.title}</h2>
+                            <p style={{position: 'relative', margin: '0', padding: '0',top: '25px', left: '25px', zIndex: '5'}}>{x.subTitle}</p>
+                            <Button data-aos="zoom-in" data-aos-duration="1500" variant="outlined" className={classes.root}>SHOP NOW</Button>
+                            <img src={x.img} alt={x} className="img-fas" />
+                        </div>
+                    </Grid>
+                    })
+                }
+            </Grid>
+        </div>
+    );
+};
+
+export default TypesFashon;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Homepage.css';
 import Banner from '../components/Banner';
+import TypesFashon from '../components/TypesFashon';
 
 const HomePage = () => {
     return (
         <div>
             <Banner />
+            <TypesFashon />
         </div>
     );
 };
