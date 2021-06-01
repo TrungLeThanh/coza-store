d110f6842ca5d75e6da0f50fa0f5f2dd818ff330
Thanh Trung <thanhtrung20022001@gmail.com>
2021-06-02 00:20:00 +0700

    products overview

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className="ui active centered inline loader" />
+    );
+};
+
+export default Loader;
--- frontend/src/components/Overview.js
+++ frontend/src/components/Overview.js
@@ -4,6 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import {useDispatch, useSelector} from 'react-redux';
 import { listProducts } from '../actions/productActions';
 import Grid from '@material-ui/core/Grid';
+import Loader from './Loader';
 
 
 
@@ -28,7 +29,7 @@ const Overview = () => {
                 {   
                     products.map((product)=>{
                         return (
-                            <Grid item key={product._id} xs={12} md={4} lg={3}>
+                            <Grid style={{overflow: 'hidden'}} item key={product._id} xs={6} md={4} lg={3}>
                                 <div className="image-style">
                                     <button className="ui button style-btn">Quick View</button>
                                     <img className="image-product" src={product.image} alt="" />
@@ -40,9 +41,87 @@ const Overview = () => {
                     })
                 }
             </Grid>
-        )
-    }
+        );
+    };
+    const productsWoman = () => {
+        return (
+            <Grid container spacing={7}>
+                {   
+                    products.map((product)=>product.category === 'woman' ?
+                        <Grid style={{overflow: 'hidden'}} item key={product._id} xs={12} md={4} lg={3}>
+                            <div className="image-style">
+                                <button className="ui button style-btn">Quick View</button>
+                                <img className="image-product" src={product.image} alt="" />
+                            </div>
+                            <p className="name-product">{product.name}</p>
+                            <p>$ {product.price}</p>
+                        </Grid>
+                        : ''
+                    )
+                }
+            </Grid>
+        );
+    };
 
+    const productsMan = () => {
+        return (
+            <Grid container spacing={7}>
+                {   
+                    products.map((product)=>product.category === 'man' ?
+                        <Grid style={{overflow: 'hidden'}} item key={product._id} xs={12} md={4} lg={3}>
+                            <div className="image-style">
+                                <button className="ui button style-btn">Quick View</button>
+                                <img className="image-product" src={product.image} alt="" />
+                            </div>
+                            <p className="name-product">{product.name}</p>
+                            <p>$ {product.price}</p>
+                        </Grid>
+                        : ''
+                    )
+                }
+            </Grid>
+        );
+    };
+
+    const productsBag = () => {
+        return (
+            <Grid container spacing={7}>
+                {   
+                    products.map((product)=>product.category === 'bag' ?
+                        <Grid style={{overflow: 'hidden'}} item key={product._id} xs={12} md={4} lg={3}>
+                            <div className="image-style">
+                                <button className="ui button style-btn">Quick View</button>
+                                <img className="image-product" src={product.image} alt="" />
+                            </div>
+                            <p className="name-product">{product.name}</p>
+                            <p>$ {product.price}</p>
+                        </Grid>
+                        : ''
+                    )
+                }
+            </Grid>
+        );
+    };
+
+    const productsWatches = () => {
+        return (
+            <Grid container spacing={7}>
+                {   
+                    products.map((product)=>product.category === 'watches' ?
+                        <Grid style={{overflow: 'hidden'}} item key={product._id} xs={12} md={4} lg={3}>
+                            <div className="image-style">
+                                <button className="ui button style-btn">Quick View</button>
+                                <img className="image-product" src={product.image} alt="" />
+                            </div>
+                            <p className="name-product">{product.name}</p>
+                            <p>$ {product.price}</p>
+                        </Grid>
+                        : ''
+                    )
+                }
+            </Grid>
+        );
+    };
 
     return (
         <div className="wrap-overview">
@@ -53,8 +132,7 @@ const Overview = () => {
                     <button onClick={() => setActive(2)} className={`ui button ${active===2 ? 'active-color' : ''}`}>Woman</button>
                     <button onClick={() => setActive(3)} className={`ui button ${active===3 ? 'active-color' : ''}`}>Man</button>
                     <button onClick={() => setActive(4)} className={`ui button ${active===4 ? 'active-color' : ''}`}>Bag</button>
-                    <button onClick={() => setActive(5)} className={`ui button ${active===5 ? 'active-color' : ''}`}>Shoes</button>
-                    <button onClick={() => setActive(6)} className={`ui button ${active===6 ? 'active-color' : ''}`}>Watches</button>
+                    <button onClick={() => setActive(5)} className={`ui button ${active===6 ? 'active-color' : ''}`}>Watches</button>
                 </div>
                 <div style={{marginTop: '-20px', position: 'relative'}} className="form-search">
                     <form noValidate autoComplete="off">
@@ -62,7 +140,19 @@ const Overview = () => {
                     </form>
                 </div>
             </div>
-            {showAllProducts()}
+            {
+                loading ?
+                <Loader /> :
+                active === 1 ?
+                showAllProducts() :
+                active === 2 ?
+                productsWoman() :
+                active === 3 ?
+                productsMan() : 
+                active === 4 ?
+                productsBag() :
+                productsWatches()
+            }
         </div>
     );
 };
