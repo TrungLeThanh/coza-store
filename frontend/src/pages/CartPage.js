92dd0a2e2d7cc8f5165f82db872eeb90c6fd9ee7
Thanh Trung <thanhtrung20022001@gmail.com>
2021-06-03 20:03:12 +0700

    cart list

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -5,6 +5,9 @@ import './CartPage.css';
 import queryString from 'query-string';
 import {Link} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 const CartPage = ({match, location}) => {
@@ -14,27 +17,61 @@ const CartPage = ({match, location}) => {
     const cart = useSelector(state => state.cart);
     const {cartItems} = cart;
 
-    let params = queryString.parse(location.search)
+    let params = queryString.parse(location.search);
 
     useEffect(() =>{
-        dispatch(addToCart(match.params.id, 5, 'M'));
-    }, [dispatch, match.params.id]);
+        dispatch(addToCart(match.params.id, params.qty, params.size));
+    }, [dispatch, match.params.id, params.qty, params.size]);
 
     const renderCart = () => {
-        return(
-            <Grid container spacing={5}>
-                <Grid style={{border: '1px solid black', width: '100%', height: '200px'}} item xs={12} sm={8} md={8} lg={8}>
-                    <Grid container spacing={0}>
-                        <Grid style={{border: '1px solid black', width: '100%', height: '100px'}} item lg={4}>
-                            <img src="" alt="" />
+        return (
+            <Grid container spacing={1}>
+                <Grid style={{border: '1px solid black', width: '100%', height: '100%'}} item lg={8}>
+                {cartItems.map((product)=>{
+                    return (
+                        <>
+                        <Grid container justify="center" spacing={0}>
+                            <Grid item xs={12} sm={6} md={4} lg={2}>
+                                <div className="image-cart">
+                                    <img src={product.image} style={{width: '100%', height: '100%'}} alt="" />
+                                </div>
+                            </Grid>
+                            <Grid style={{border: '1px solid black', width: '100%', height: '100px'}} item xs={12} sm={6} md={4} lg={2}>
+                                {product.name}
+                            </Grid>
+                            <Grid style={{border: '1px solid black', width: '100%', height: '100px'}} item xs={12} sm={6} md={4} lg={2}>
+                                $: {product.price}
+                            </Grid>
+                            <Grid style={{border: '1px solid black', width: '100%', height: '100px'}} item xs={12} sm={6} md={4} lg={2}>
+                                <FormControl style={{width: '100px', paddingLeft: '15px'}}>
+                                    <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={product.qty}
+                                    onChange={(e) => dispatch(addToCart(product.product, e.target.value, product.size))}
+                                    >
+                                        {[...Array(product.countInStock).keys()].map((x) => (
+                                            <MenuItem value={x + 1} key={x + 1}>
+                                                {x + 1}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid style={{border: '1px solid black', width: '100%', height: '100px'}} item xs={12} sm={6} md={4} lg={2}>
+                                
+                            </Grid>
+                            <Grid style={{border: '1px solid black', width: '100%', height: '100px'}} item xs={12} sm={6} md={4} lg={2}>
+                                
+                            </Grid>
                         </Grid>
-                        <Grid style={{border: '1px solid black', width: '100%', height: '100px'}} item lg={4}>
+                        <hr />
+                        </>
+                    )
+                })}
+                </Grid>
+                <Grid style={{border: '1px solid black', width: '100%', height: '100px'}} item lg={4}>
 
-                        </Grid>
-                        <Grid style={{border: '1px solid black', width: '100%', height: '100px'}} item lg={4}>
-
-                        </Grid>
-                    </Grid>
                 </Grid>
             </Grid>
         );
