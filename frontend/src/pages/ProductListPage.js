6d099465442007067ef171624d6aebaffb11c7e4
Thanh Trung <thanhtrung20022001@gmail.com>
2021-06-28 17:36:38 +0700

    modal delete product

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './ProductListPage.css';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import {Table} from 'react-bootstrap';
+import {Table, Modal, Button} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 import {Link} from 'react-router-dom';
 import {listProducts, deleteProduct} from '../actions/productActions';
@@ -23,6 +23,9 @@ const ProductListPage = ({history}) => {
     let count = 1;
 
     // MODAL CONFIRM
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const [id, setId] = useState('');
 
     useEffect(() => {
         if(userInfor && userInfor.isAdmin){
@@ -33,10 +36,14 @@ const ProductListPage = ({history}) => {
         }
     }, [dispatch, history, userInfor, successDelete]);
 
-    const deleteHandler = (id) => {
-        if(window.confirm('Are you sure you want to delete')){
-            dispatch(deleteProduct(id));
-        }
+    const confirmDelete = (id) => {
+        setShow(true);
+        setId(id);
+    };
+
+    const deleteHandler = () => {
+        dispatch(deleteProduct(id));
+        setShow(false);
     };
 
     return (
@@ -51,7 +58,7 @@ const ProductListPage = ({history}) => {
             <div className="create">
                 <h2 style={{fontWeight: '500'}}>Current products</h2>
                 <button className="ui linkedin button">
-                    <i class="plus icon"></i>
+                    <i className="plus icon"></i>
                     CREATE
                 </button>
             </div>
@@ -86,7 +93,7 @@ const ProductListPage = ({history}) => {
                                     <LinkContainer to={`/admin/product/${product._id}/edit`}>
                                         <i className='fas fa-edit'></i>
                                     </LinkContainer> | 
-                                    <i style={{marginLeft: '5px', color: '#D32F41'}} onClick={() => deleteHandler(product._id)} className='fas fa-trash'></i>
+                                    <i style={{marginLeft: '5px', color: '#D32F41'}} onClick={() => confirmDelete(product._id)} className='fas fa-trash'></i>
                                 </td>
                                 </tr>
                             ))}
@@ -94,6 +101,26 @@ const ProductListPage = ({history}) => {
                     </Table>
                 )
             }
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                style={{zIndex: '99999'}}
+                >
+                <Modal.Header>
+                <Modal.Title>COZA STORE</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                Are you sure delete it?
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="danger" onClick={() => deleteHandler()}>Delete</Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
